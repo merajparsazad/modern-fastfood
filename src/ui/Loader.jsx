6330e940629ac18d3a00e423b2b73aeba7e6c43d
Meraj Parsazad <merajparsazad@gmail.com>
2025-09-04 14:40:59 +0330

    Set up styled components, add color palettes and media queries, and start styling with Loader.jsx

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,45 @@
+import styled, { keyframes } from "styled-components";
+
+const loading = keyframes`
+  20% {
+    background-position: 0% 0%, 50% 50%, 100% 50%;
+  }
+  40% {
+    background-position: 0% 100%, 50% 0%, 100% 50%;
+  }
+  60% {
+    background-position: 0% 50%, 50% 100%, 100% 0%;
+  }
+  80% {
+    background-position: 0% 50%, 50% 50%, 100% 100%;
+  }
+`;
+
+const StyledLoader = styled.div`
+  position: absolute;
+  background-color: var(--color-stone-300 0.2);
+  backdrop-filter: blur(2px);
+  inset: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & div {
+    width: 45px;
+    aspect-ratio: 0.75;
+    --c: no-repeat linear-gradient(var(--color-stone-800) 0 0);
+    background: var(--c) 0% 50%, var(--c) 50% 50%, var(--c) 100% 50%;
+    background-size: 20% 50%;
+    animation: ${loading} 1s infinite linear;
+  }
+`;
+
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <StyledLoader>
+      <div></div>
+    </StyledLoader>
+  );
 }
 
 export default Loader;
---
+++ src/utils/media-queries.js
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+const breakpoints = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+  "2xl": "1536px",
+};
+
+const media = Object.keys(breakpoints).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${breakpoints[label]}) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
+
+export default media;
